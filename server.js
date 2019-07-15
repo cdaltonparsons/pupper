8e19bbbb988c41fbc104f9bbadc28123c4f0ec7a
cmoney <moneymaker.carolyn@gmail.com>
2019-07-15 14:52:49 -0600

    initial handlebars/html work

---
+++ server.js
@@ -0,0 +1,26 @@
+// *** Dependencies
+// =============================================================
+var express = require("express");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
+
+// Start our server so that it can begin listening to client requests.
+db.sequelize.sync( { force: true }).then(function(){
+  app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+})
