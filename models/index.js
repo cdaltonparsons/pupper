213021c827f6b790c1bf728d3632533adcfe91a4
Charlie Parsons <cdaltonparsons@gmail.com>
2019-07-16 09:06:23 -0600

    tried tying in some of the auth stuff i was working on

--- models/index.js
+++ models/index.js
@@ -17,10 +17,10 @@ if (config.use_env_variable) {
 
 fs
   .readdirSync(__dirname)
-  .filter(file => {
+  .filter(function(file) {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
-  .forEach(file => {
+  .forEach(function(file) {
     const model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
