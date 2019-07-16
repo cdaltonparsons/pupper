1d92947c51f1a411a4410743a3d3c09895fa7c1a
Charlie Parsons <cdaltonparsons@gmail.com>
2019-07-16 11:13:17 -0600

    added passport.js config file, auth-routes file, and added some res.renders to the controller

--- controllers/pupper_controller.js
+++ controllers/pupper_controller.js
@@ -1,1 +1,22 @@
+var exports = module.exports = {}
+// these are the res.renders for our sign up, sign in and then the protected page, called here dashboard
+exports.signup = function(req,res){
 
+	res.render('signup'); 
+}
+
+exports.signin = function(req,res){
+
+	res.render('signin'); 
+}
+
+exports.dashboard = function(req,res){
+	res.render('dashboard'); 
+}
+
+exports.logout = function(req,res){
+  req.session.destroy(function(err) {
+  res.redirect('/');
+  });
+
+}
--- models/user.js
+++ models/user.js
@@ -1,15 +1,15 @@
-module.exports = function(sequelize, Sequelize) {
+module.exports = function(sequelize, DataTypes) {
 
-	var User = sequelize.define('user', {
-		id: { autoIncrement: true, primaryKey: true, type: Sequelize.INTEGER},
-		firstname: { type: Sequelize.STRING,notEmpty: true},
-		lastname: { type: Sequelize.STRING,notEmpty: true},
-		username: {type:Sequelize.TEXT},
-		about : {type:Sequelize.TEXT},
-		email: { type:Sequelize.STRING, validate: {isEmail:true} },
-		password : {type: Sequelize.STRING,allowNull: false }, 
-		last_login: {type: Sequelize.DATE},
-        status: {type: Sequelize.ENUM('active','inactive'),defaultValue:'active' }
+	var User = sequelize.define('User', {
+		id: { autoIncrement: true, primaryKey: true, type: DataTypes.INTEGER},
+		name: { type: DataTypes.STRING,notEmpty: true},
+		lastname: { type: DataTypes.STRING,notEmpty: true},
+		username: {type: DataTypes.TEXT},
+		about : {type: DataTypes.TEXT},
+		email: { type: DataTypes.STRING, validate: {isEmail:true} },
+		password : {type: DataTypes.STRING,allowNull: false }, 
+		last_login: {type: DataTypes.DATE},
+        status: {type: DataTypes.ENUM('active','inactive'),defaultValue:'active' }
 
 });
 
