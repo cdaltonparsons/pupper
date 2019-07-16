ab5a27de0947472bb29940ccb6bcaa571a1c4eca
cmoney <moneymaker.carolyn@gmail.com>
2019-07-16 16:25:49 -0600

    added footer

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,19 +6,16 @@ var path = require("path");
 // =============================================================
 module.exports = function(app) {
 
-  // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads view.html
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "./home.html"));
   });
 
-  // Route to the cms page
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "./main-survey.html"));
   });
 
-  // blog route loads blog.html
+
   app.get("/match", function(req, res) {
     res.sendFile(path.join(__dirname, "./match-survey.html"));
   });
