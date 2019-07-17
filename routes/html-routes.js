f666e7a55038db433019b67e120bfac6d0a42d08
Charlie Parsons <cdaltonparsons@gmail.com>
2019-07-17 09:14:21 -0600

    protected survey and match routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -14,22 +14,22 @@ module.exports = function(app) {
   });
 
   // Route to the cms page
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/main-survey.html"));
-  });
+  // app.get("/survey", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/main-survey.html"));
+  // });
 
   // blog route loads blog.html
-  app.get("/match", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/match-survey.html"));
-  });
+  // app.get("/match", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/match-survey.html"));
+  // });
 
   // sign up route loads signup.html
   app.get("/signup", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
-  app.get("/login", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+  app.get("/signin", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/signin.html"));
   });
 
 
--- server.js
+++ server.js
@@ -17,24 +17,29 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // for passport
-// app.use(
-//   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
-// ); // session secret
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+); // session secret
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Static directory
 // app.use(express.static("public"));
 
-require("./routes/html-routes.js")(app)
+require("./routes/html-routes.js")(app, passport)
 require("./routes/app-api-routes.js")(app)
-require("./routes/auth-routes.js")(app)
-require("./config/passport/passport.js")(passport, db.user);
+require("./routes/auth-routes.js")(app, passport)
+require("./config/passport/passport.js")(passport, db.User);
 
 // Start our server so that it can begin listening to client requests.
-db.sequelize.sync( { force: true }).then(function(){
+// deleted {force: true} from the () in sync
+db.sequelize.sync().then(function(){
   app.listen(PORT, function() {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
   });
 })
+
+
+
+//inside .env, need TOKEN_SECRET=whatever   ---- must match exactly whenver its used
