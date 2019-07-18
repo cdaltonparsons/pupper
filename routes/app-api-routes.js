984339cad0dcad3ee30594960eebcf76c302d5dc
Claudia Falk <claudiafalk0@gmail.com>
2019-07-17 21:05:40 -0600

    updating handlebars

--- routes/app-api-routes.js
+++ routes/app-api-routes.js
@@ -13,34 +13,51 @@ module.exports = function (app) {
     });
 
     // Adding to match table
-    app.post("/api/match", function (req, res) {
-        db.Match.create(req.body).then(function (match) {
-            res.json(match)
-        });
-    });
+    // app.post("/api/match", function (req, res) {
+    //     db.Match.create(req.body).then(function (match) {
+    //         res.json(match)
+    //     });
+    // });
 
     // Getting data from tables to show matches on front end
 
-        app.get("/api/matches/:size/:energetic/:dominant", function(req, res) {
-            
-            console.log(req.params.size);
-            console.log(req.params.energetic);
-            console.log(req.params.dominant);
-            
-            db.Pupper.findAll({
-                
-                where: {
-                    size: req.params.size,
-                    energetic: req.params.energetic,
-                    dominant: req.params.dominant
+        app.get("/api/matches/:size/:energetic/:dominant", function(req, res){
+            var matchArr = {
+                []};
+            for(i = 0; i < db.Pupper.length; i++){
+                if(db.Pupper[i].size === req.params.size && pupper[i].energetic === req.params.energetic && pupper[i].dominant === req.params.energetic){
+                    matchArr.push(db.Pupper[i]);
+                }else if(db.Pupper[i].size === req.params.size && pupper[i].energetic === req.params.energetic){
+                    matchArr.push(db.Pupper[i])
+                }else if(db.Pupper[i].size === req.params.size){
+                    matchArr.push(db.Pupper[i])
                 }
-            }).then(function(data){
-                res.json(data);
-                $('#owner').text(data.ownerName);
-                $('#match-name').text(data.dogName);
-                $('#image').attr(data.image);
+            }
+            console.log('Matcharr' + matchArr);
+            res.render('index', matchArr)
+        })
+        
+    };
+        // console.log(req.params.size);
+
+            // console.log(req.params.energetic);
+            // console.log(req.params.dominant);
+            
+            // db.Pupper.findAll({
                 
-            });
+            //     where: {
+            //         size: req.params.size,
+            //         energetic: req.params.energetic,
+            //         dominant: req.params.dominant
+            //     }
+            // }).then(function(data){
+
+
+            //     console.log('This is the data for the get' + data);
+
+            //     // console.log("this is the data from the get" + data.ownerName + data.dogName + data.image);
+                
+            // });
             
             
             // var query = {};
@@ -64,8 +81,6 @@ module.exports = function (app) {
                                 //     res.json(data);
                                 // });
                                 
-        });
-    }
 
 
     // PUT route for updating posts
